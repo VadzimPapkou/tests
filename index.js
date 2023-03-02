688e0d8fd2ff198fa699232c040bdced46f618ee
Vadim <playvarian@gmail.com>
2023-03-02 21:48:41 +0300

    init pharma

--- index.js
+++ index.js
@@ -2,13 +2,29 @@ const fs = require("fs")
 
 const text = fs.readFileSync("test.txt").toString();
 
-const answers = text.split(/\n{2,}/gi);
-const questions = answers.map(aw => aw.replace(/{.*?}/g, "").replace(/[+-]/gi, "? "));
-const questionsAnswers = answers.map((answer, index) => ({answer, question: questions[index], index}))//.slice(192, 213);
 
-fs.writeFileSync("test.json", JSON.stringify(questions.map((q, i) => i + "=" + q), null, 2));
+const chunks = text.split(/(\r\n){2,}/gi).map(chunk => ({chunk, fulltext: chunk.match(/\n/gi)?.length === 2}));
 
-let currentQuestion = questionsAnswers[0];
+const questions = chunks.map((chunkWithMetadata, index) => {
+  if(chunkWithMetadata.fulltext) {
+    const [question, answer] = chunkWithMetadata.split(/n/gi);
+    return {
+      question: "fulltext" + question,
+      answer,
+      id: index
+    }
+  } else {
+    return {
+      question: chunkWithMetadata.chunk,
+      answer: chunkWithMetadata.chunk.replace(/^[+-]/gi, "? "),
+      id: index
+    }
+  }
+});
+
+fs.writeFileSync("test.json", JSON.stringify(questions.map((q) => q.id + ": " + q.question), null, 2));
+
+let currentQuestion = questions[0];
 setText(currentQuestion.question)
 
 function setText(text) {
@@ -19,45 +35,43 @@ function setText(text) {
 process.stdin.on("data", data => {
   data = data.toString().toLowerCase().replace(/\s/, "");
 
-  console.log(data)
-
   let prefix;
 
   switch (data) {
     // random
     case "r":
-      currentQuestion = getRandom(questionsAnswers);
-      prefix = currentQuestion.index + " of " + questionsAnswers.length + "\n";
+      currentQuestion = getRandom(questions);
+      prefix = currentQuestion.id + " of " + questions.length + "\n";
       setText(prefix + currentQuestion.question)
       break;
     //answer
     case "a":
-      prefix = currentQuestion.index + " of " + questionsAnswers.length + "\n";
+      prefix = currentQuestion.id + " of " + questions.length + "\n";
       setText(prefix + currentQuestion.answer);
       break;
     // next
     case "n":
-      currentQuestion = questionsAnswers[currentQuestion.index + 1];
-      prefix = currentQuestion.index + " of " + questionsAnswers.length + "\n";
+      currentQuestion = questions[currentQuestion.id + 1];
+      prefix = currentQuestion.id + " of " + questions.length + "\n";
       setText(prefix + currentQuestion.question)
       break;
     // previous
     case "p":
-      currentQuestion = questionsAnswers[currentQuestion.index - 1];
-      prefix = currentQuestion.index + " of " + questionsAnswers.length + "\n";
+      currentQuestion = questions[currentQuestion.id - 1];
+      prefix = currentQuestion.id + " of " + questions.length + "\n";
       setText(prefix + currentQuestion.question)
   }
 
   if (data.startsWith("i")) {
     console.clear();
-    const index = +data.replace(/\D/gi, "")
-    global.qa = questionsAnswers[index];
+    const id = +data.replace(/\D/gi, "")
+    global.qa = questions[id];
     console.log(global.qa.question)
   }
 
   if(data.startsWith("r") && data.endsWith("a")) {
     [from, to] = data.split("-").map(x => +x.replace(/\D/gi, ""));
-    currentQuestion = getRandom(questionsAnswers.slice(from, to));
+    currentQuestion = getRandom(questions.slice(from, to));
     setText(currentQuestion.question)
   }
 });
@@ -66,4 +80,3 @@ function getRandom(arr) {
   const index = Math.floor(Math.random() * arr.length);
   return arr.splice(index, 1)[0];
 }
-
